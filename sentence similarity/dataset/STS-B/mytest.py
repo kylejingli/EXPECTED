42b2dfc2a75f676a2b027c93b1205ad20ca0df16
Jing Li (Kyle) <33900490+kylejingli@users.noreply.github.com>
2023-07-06 20:49:56 +0800

    Add files via upload

---
+++ sentence similarity/dataset/STS-B/mytest.py
@@ -0,0 +1,19 @@
+import os
+import glob
+import pandas as pd
+from csv import QUOTE_NONE
+os.chdir("./")
+
+df = ({'A', [1,2,3], 'B',})
+
+#combine all files in the list
+combined_tsv = pd.concat([pd.read_csv(f, sep='\t', quoting=QUOTE_NONE) for f in all_filenames], ignore_index=True)
+combined_tsv['index'] = range(combined_tsv.shape[0])
+#export to csv
+combined_tsv.to_csv( "all.tsv",  sep='\t', index_label=False, encoding='utf-8-sig', quoting=QUOTE_NONE)
+
+'''
+data1=pd.read_csv("all.tsv", sep='\t', quoting=QUOTE_NONE )
+data2=pd.read_csv("all.tsv", sep='\t', index_col=0, quoting=QUOTE_NONE )
+print(data1)
+'''
